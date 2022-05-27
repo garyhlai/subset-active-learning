6c818133a70204b6fee1ea28747515f3e750f262
ghlai9665 <glai9665@gmail.com>
2022-05-27 00:36:04 -0500

    refactor: modularize code & add tests

---
+++ src/subset_classifier.py
@@ -0,0 +1,108 @@
+from transformers import AutoTokenizer
+import json
+import sqlite3
+import pandas as pd
+from collections import Counter
+from pydantic.dataclasses import dataclass
+from typing import List
+
+import logging
+from tabulate import tabulate
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+@dataclass(frozen=True)
+class OptimalSubsetClassifierConfig:
+    max_length: int = 66
+    debug: bool = False
+    model_name: str = "google/electra-small-discriminator"
+    batch_size: int = 8
+
+
+def get_df_from_db(db_path):
+    with sqlite3.connect(db_path) as conn:
+        # Now in order to read in pandas dataframe we need to know table name
+        cursor = conn.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        table_name = cursor.fetchall()[0][0]
+        df = pd.read_sql_query(f"SELECT * FROM {table_name}", conn)
+    return df
+
+
+def get_optimal_subset_data_indices(df):
+    optimal_subset_idx = df["objective"].idxmax()
+
+    optimal_subset_data_indices = set(json.loads(df.iloc[optimal_subset_idx].indexes))
+    return optimal_subset_data_indices
+
+
+def get_subset_unique_counts(df):
+    # count the unique number of data points in each subset
+    indexes = list(map(json.loads, df["indexes"].to_list()))
+    unique_index_counts = list(map(lambda x: len(set(x)), indexes))
+    subset_sizes = Counter(unique_index_counts)
+    return subset_sizes
+
+
+def preprocess(data, config, optimal_subset_data_indices):
+    tokenizer = AutoTokenizer.from_pretrained(config.model_name)
+
+    def tokenize_func(examples, idx):
+        tokenized = tokenizer(
+            examples["sentence"], padding="max_length", max_length=config.max_length, truncation=True
+        )
+        tokenized["labels"] = 1 if idx in optimal_subset_data_indices else 0
+        return tokenized
+
+    ds = data.map(tokenize_func, remove_columns=data.column_names, batched=False, with_indices=True)
+
+    ds.set_format(type="torch")
+
+    return ds
+
+
+def create_stratefied_split(
+    positive_indices: List[str], negative_indices: List[str], split_points: float = (0.8, 0.9)
+):
+    """Split the dataset while keeping the overall class ratio (positive to negative) the same"""
+    optimal_subset_data_indices_ls = list(positive_indices)
+    non_optimal_subset_data_indices_ls = list(negative_indices)
+
+    # get the positive examples
+    split_indices = (
+        round(len(optimal_subset_data_indices_ls) * split_points[0]),
+        round(len(optimal_subset_data_indices_ls) * split_points[1]),
+    )
+
+    train_pos_indices = optimal_subset_data_indices_ls[: split_indices[0]]
+    valid_pos_indices = optimal_subset_data_indices_ls[split_indices[0] : split_indices[1]]
+    test_pos_indices = optimal_subset_data_indices_ls[split_indices[1] :]
+
+    # get the negative examples
+    split_indices = (
+        round(len(non_optimal_subset_data_indices_ls) * split_points[0]),
+        round(len(non_optimal_subset_data_indices_ls) * split_points[1]),
+    )
+
+    train_neg_indices = non_optimal_subset_data_indices_ls[: split_indices[0]]
+    valid_neg_indices = non_optimal_subset_data_indices_ls[split_indices[0] : split_indices[1]]
+    test_neg_indices = non_optimal_subset_data_indices_ls[split_indices[1] :]
+
+    logger.info(
+        tabulate(
+            [
+                ["train", len(train_pos_indices), len(train_neg_indices)],
+                ["valid", len(valid_pos_indices), len(valid_neg_indices)],
+                ["test", len(test_pos_indices), len(test_neg_indices)],
+            ],
+            headers=["dataset", "num positive examples", "num negative examples"],
+        )
+    )
+    # combine
+    train_indices = train_pos_indices + train_neg_indices
+    valid_indices = valid_pos_indices + valid_neg_indices
+    test_indices = test_pos_indices + test_neg_indices
+    return train_indices, valid_indices, test_indices
---
+++ tests/test_subset_classifier.py
@@ -0,0 +1,44 @@
+from src.subset_classifier import (
+    create_stratefied_split,
+    get_df_from_db,
+    get_optimal_subset_data_indices,
+    OptimalSubsetClassifierConfig,
+    preprocess,
+)
+from datasets import load_dataset
+import pytest
+
+
+@pytest.fixture
+def optimal_subset_data_indices():
+    df = get_df_from_db("./subset_selection/sst_results.db")
+    optimal_subset_data_indices = get_optimal_subset_data_indices(df)
+    return optimal_subset_data_indices
+
+
+def test_preprocess(optimal_subset_data_indices):
+    # Set up
+    sst2 = load_dataset("sst")
+    data_pool = sst2["train"].shuffle(seed=0).select(range(1000))
+    config = OptimalSubsetClassifierConfig(
+        max_length=66, debug=False, model_name="google/electra-small-discriminator", batch_size=8
+    )
+    # Run
+    ds = preprocess(data_pool, config, optimal_subset_data_indices)
+    # test the labels are correct
+    for idx, label in enumerate(ds["labels"]):
+        if idx in optimal_subset_data_indices:
+            assert int(label) == 1
+        else:
+            assert int(label) == 0
+
+
+def test_create_stratefied_split(optimal_subset_data_indices):
+    data_pool_indices = list(range(1000))
+    non_optimal_subset_data_indices = list(set(data_pool_indices) - optimal_subset_data_indices)
+    optimal_subset_data_indices = list(optimal_subset_data_indices)
+    train_indices, valid_indices, test_indices = create_stratefied_split(
+        optimal_subset_data_indices, non_optimal_subset_data_indices, split_points=(0.8, 0.9)
+    )
+    # check there are no overlaps between the three datasets
+    assert len(set(train_indices + valid_indices + test_indices)) == len(train_indices + valid_indices + test_indices)
