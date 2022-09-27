db24f23dc95a76eceb2747d0c859a5f5ce6cfeff
johntzwei <johntzwei@yahoo.com>
2022-09-27 22:09:38 +0000

    add example script for sbatch (sending job into our computing cluster)

---
+++ scripts/subset_selection/mnli_search.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=simulated-annealing
+#SBATCH --gres=gpu:1
+#SBATCH --time=0-72:00
+#SBATCH --ntasks=1
+#SBATCH --array=1-30
+#SBATCH --qos=general
+#SBATCH --requeue
+
+source activate torch
+
+papermill -p db_path ./mnli_9-27 \
+    -p ds_name mnli \
+    -p wandb_project mnli_search \
+    -p wandb_entity johntzwei \
+    -p annealing_runs 2000 \
+    -p pool_size 2000 \
+    -p search_size 200 \
+    -p max_steps 3000 \
+    ./search_subset.ipynb /dev/null &
+
+papermill -p db_path ./mnli_9-27 \
+    -p ds_name mnli \
+    -p wandb_project mnli_search \
+    -p wandb_entity johntzwei \
+    -p annealing_runs 2000 \
+    -p pool_size 2000 \
+    -p search_size 200 \
+    -p max_steps 3000 \
+    ./search_subset.ipynb /dev/null &
+
+wait
